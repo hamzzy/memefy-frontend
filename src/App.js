18836bbb865b4cc8b92e38a9c19d993faf6a73d5
Worldsalt3 <riliwanhazzan@gmail.com>
2020-08-12 17:40:50 +0100

    add reset password route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import ResetPassword from "./Reset/Reset";
 import Dashboard from "./pages/Dashboard/dashboard";
 import Search from "./pages/SearchPage";
 function App() {
@@ -22,6 +23,7 @@ function App() {
 
           <Route path="/signUp" component={Register}/>
 
+          <Route path="/resetpassword" component={ResetPassword} /> 
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/search'  component={Search}/>
 
