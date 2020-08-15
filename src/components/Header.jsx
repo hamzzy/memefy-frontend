d274b852e651106adbf540bb348da0d061f26b1f
Worldsalt3 <riliwanhazzan@gmail.com>
2020-08-15 11:45:04 +0100

    Home Page Build Complete, Fully responsive.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {Navbar,Nav,Button} from 'react-bootstrap';
-import logo from '../assets/images/logo.svg'
+import logo from '../assets/images/logo.svg';
+import mobileLogo from '../assets/images/mobile_Logo.svg';
 import {Link} from "react-router-dom";
 export class Header extends Component {
     render() {
         return (
             <div className="font">
-                <Navbar expand="lg ml-4">
+                <Navbar expand="lg ml-lg-4">
                   <Navbar.Brand href="#home">
+                    <img
+                        alt="Memefy Logo"
+                        src={mobileLogo}
+                        className="d-inline-block d-sm-inline-block d-md-none d-lg-none d-xl-none align-top"
+                        fluid
+                    />{' '}
                     <img
                       alt="Memefy Logo"
                       src={logo}
                     
-                      className="d-inline-block align-top"
+                      className="d-none d-sm-none d-md-inline-block d-lg-inline-block d-xl-inline-block align-top"
                     />{' '}
                   </Navbar.Brand>
                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -21,7 +28,7 @@ export class Header extends Component {
                       <Nav.Link href="#home" className="mx-3 navb">Explore</Nav.Link>
                       <Nav.Link  href="#link" className="mx-3 navb">API</Nav.Link>
                       <Nav.Link href="/login" className="mx-3 navb">Login</Nav.Link>
-                      <Link to='/signup' className="mt-1 ml-3"><Button type="submit" className="fs-20 btn-bg px-4" size="lg">Sign Up</Button></Link>
+                      <Link to='/signup' className="mt-1 ml-3"><Button type="submit" className="fs-20 btn-bg px-4 rounded-lg" size="lg">Sign Up</Button></Link>
                     </Nav>
                   </Navbar.Collapse>
                 </Navbar>
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,10 @@ import img from '../assets/images/meme.svg'
 export class footer extends Component {
     render() {
         return (
-            <div>
-                <div className='text-center mt-5'>
-                    <img src={img} alt=""/>
-                    <p>Built with love by Hamzat and soliudeen </p>
+            <div className="font bg h-50">
+                <div className='text-center mt-5 color-1 fs-24'>
+                    <img src={img} alt="Memefy Logo"/>
+                    <p className="sm-24">&copy; 2020 Memefy.</p>
                 </div>
                 
                 
